40ecd9c93775aa7364025a2e48b1acc35d6af6ac
kelieCn <keliecn720@gmail.com>
2025-10-22 23:35:55 +0800

    fix: vue 子应用增加路由跳转功能

---
+++ packages/micro-front-end/qiankun-test/child-vue/src/CardDetail.vue
@@ -0,0 +1,49 @@
+<template>
+  <Descriptions bordered>
+    <DescriptionsItem label="Product">
+      Cloud Database
+    </DescriptionsItem>
+    <DescriptionsItem label="Billing Mode">
+      Prepaid
+    </DescriptionsItem>
+    <DescriptionsItem label="Automatic Renewal">
+      YES
+    </DescriptionsItem>
+    <DescriptionsItem label="Order time">
+      2018-04-24 18:00:00
+    </DescriptionsItem>
+    <DescriptionsItem label="Usage Time" :span="2">
+      2019-04-24 18:00:00
+    </DescriptionsItem>
+    <DescriptionsItem label="Status" :span="3">
+      <Badge status="processing" text="Running" />
+    </DescriptionsItem>
+    <DescriptionsItem label="Negotiated Amount">
+      $80.00
+    </DescriptionsItem>
+    <DescriptionsItem label="Discount">
+      $20.00
+    </DescriptionsItem>
+    <DescriptionsItem label="Official Receipts">
+      $60.00
+    </DescriptionsItem>
+    <DescriptionsItem label="Config Info">
+      Data disk type: MongoDB
+      <br>
+      Database version: 3.4
+      <br>
+      Package: dds.mongo.mid
+      <br>
+      Storage space: 10 GB
+      <br>
+      Replication factor: 3
+      <br>
+      Region: East China 1
+      <br>
+    </DescriptionsItem>
+  </Descriptions>
+</template>
+
+<script setup lang="ts">
+  import { Descriptions, DescriptionsItem, Badge } from 'ant-design-vue'
+</script>
---
+++ packages/micro-front-end/qiankun-test/child-vue/src/CardList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card-list">
+    <Row :gutter="20">
+      <Col
+        v-for="(url, index) in images"
+        :key="url"
+        :span="6"
+      >
+        <Card
+          hoverable
+          style="width: 100%;"
+          @click="onClick(index)"
+        >
+          <template #cover>
+            <img alt="example" :src="url">
+          </template>
+          <CardMeta title="Card title" description="This is the description">
+            <template #avatar>
+              <Avatar :src="avatar" />
+            </template>
+          </CardMeta>
+        </Card>
+      </Col>
+    </Row>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { useRouter } from 'vue-router'
+  import { Card, Row, Col, CardMeta, Avatar } from 'ant-design-vue'
+  import image1 from './assets/image1.png'
+  import image2 from './assets/image2.png'
+  import image3 from './assets/image3.png'
+  import image4 from './assets/image4.png'
+  import avatar from './assets/avatar1.png'
+
+  const images = [
+    image1,
+    image2,
+    image3,
+    image4,
+  ]
+
+  const router = useRouter()
+  function onClick(index: number) {
+    router.push({
+      name: 'CardDetail',
+      params: { index },
+    })
+  }
+</script>
